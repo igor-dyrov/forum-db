1ee6e8a9a710c3acfd42248931b41aba2c6dc431
Igor Dyrov <dyrov.ig@yandex.ru>
2019-01-03 23:41:01 +0300

    update(threadHandler.go): prettify CreateThread

--- src/handlers/threadHandler.go
+++ src/handlers/threadHandler.go
@@ -17,61 +17,33 @@ import (
 )
 
 func CreateThread(w http.ResponseWriter, request *http.Request) {
-	b, err := ioutil.ReadAll(request.Body)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
+	body, err := ioutil.ReadAll(request.Body)
 	defer request.Body.Close()
+	PanicIfError(err)
+
 	var thread models.Thread
-	err = json.Unmarshal(b, &thread)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
+	err = json.Unmarshal(body, &thread)
+	PanicIfError(err)
+
 	thread.Forum = mux.Vars(request)["slug"]
-	db := common.GetDB()
 
 	if !getters.UserExists(thread.Author) {
-		var message models.ResponseMessage
-		message.Message = "Can't find thread author by nickname: " + thread.Author
-		output, err := json.Marshal(message)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		w.Header().Set("content-type", "application/json")
-		w.WriteHeader(404)
-		w.Write(output)
+		WriteNotFoundMessage(w, "Can't find thread author by nickname: "+thread.Author)
 		return
 	}
 
 	if !getters.SlugExists(thread.Forum) {
-		var message models.ResponseMessage
-		message.Message = "Can't find thread forum by slug: " + thread.Forum
-		output, err := json.Marshal(message)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		w.Header().Set("content-type", "application/json")
-		w.WriteHeader(404)
-		w.Write(output)
+		WriteNotFoundMessage(w, "Can't find thread forum by slug: "+thread.Forum)
 		return
 	}
 
 	var gotThread = getters.GetThreadBySlug(thread.Slug)
 	if gotThread.Slug != "" {
-		output, err := json.Marshal(gotThread)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		w.Header().Set("content-type", "application/json")
-		w.WriteHeader(409)
-		w.Write(output)
+		WriteResponce(w, 409, gotThread)
 		return
 	}
+
+	db := common.GetDB()
 
 	if thread.Slug != "" {
 		db.QueryRow("INSERT INTO threads (slug, created, message, title, author, forum, votes) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
@@ -80,26 +52,13 @@ func CreateThread(w http.ResponseWriter, request *http.Request) {
 		db.QueryRow("INSERT INTO threads (created, message, title, author, forum, votes) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
 			thread.Created, thread.Message, thread.Title, thread.Author, thread.Forum, thread.Votes).Scan(&thread.ID)
 	}
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
 
 	_, err = db.Exec("UPDATE forums SET threads = threads + 1 WHERE slug = $1", thread.Forum)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
+	PanicIfError(err)
 
 	thread.Forum = getters.GetSlugCase(thread.Forum)
-	output, err := json.Marshal(thread)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	w.Header().Set("content-type", "application/json")
-	w.WriteHeader(201)
-	w.Write(output)
+
+	WriteResponce(w, 201, thread)
 }
 
 func GetThreads(w http.ResponseWriter, request *http.Request) {
